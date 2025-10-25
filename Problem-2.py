b6eaa0b651c85f63af005622568ae41e1a168de0
Sarojini Kunasani <sarojini.kunasani04@gmail.com>
2025-10-25 15:38:48 +0530

    Add  code to problem-2
    This script takes a user input for a number and generates the first 'a' odd numbers, printing them in a comma-separated format.

--- Problem-2.py
+++ Problem-2.py
@@ -1,1 +1,6 @@
-
+# Take input from user
+a = int(input("Enter a number: "))
+# Generate the series of first 'a' odd numbers
+series = [str(2*i + 1) for i in range(a)]
+# Print the series separated by commas
+print(", ".join(series))
