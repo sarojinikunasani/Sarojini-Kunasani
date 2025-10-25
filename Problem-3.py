9c04c9a22763d20f3aacfb2b9b86fe423e8dcebe
Sarojini Kunasani <sarojini.kunasani04@gmail.com>
2025-10-25 15:42:01 +0530

    Add code to problem-3

--- Problem-3.py
+++ Problem-3.py
@@ -1,1 +1,11 @@
-
+# Take input from user
+a = int(input("Enter a number: "))
+# Determine the count of odd numbers to print
+if a % 2 == 0:
+    count = a - 1  # for even a, use previous odd number
+else:
+    count = a      # for odd a, use a itself
+# Generate the series
+series = [str(2*i + 1) for i in range(count)]
+# Print the series separated by commas
+print(", ".join(series))
