d41907a02f67b37404bb34d12c7a4c0a5343ccb0
Sarojini Kunasani <sarojini.kunasani04@gmail.com>
2025-10-25 15:44:13 +0530

    Add code to problem-4

--- Problem-4.py
+++ Problem-4.py
@@ -1,1 +1,15 @@
+# Take input from user
+a = int(input("Enter a number: "))
+
+# Determine how many odd numbers to generate
+if a % 2 == 0:
+    count = a - 1  # for even numbers, use previous odd series length
+else:
+    count = a      # for odd numbers, series length = a
+  
+# Generate the series of odd numbers
+series = [str(2 * i - 1) for i in range(1, count + 1)]
+
+# Print the series with comma
+print(", ".join(series))
 
